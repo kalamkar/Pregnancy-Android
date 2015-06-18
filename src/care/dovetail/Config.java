95783af35d8b1dd1392f52df4a235bc47b0e5520
Abhijit Kalamkar <abhijit@gmail.com>
2015-06-18 10:57:53 -0700

    Added url for profile photo view

--- src/care/dovetail/Config.java
+++ src/care/dovetail/Config.java
@@ -26,6 +26,9 @@ public class Config extends care.dovetail.common.Config {
 	public static final String MESSAGE_URL  = API_URL + "/message";
 	public static final String SEARCH_URL = API_URL + "/search";
 
+	public static final String PHOTO_URL =
+			"http://commondatastorage.googleapis.com/dovetail-profiles/";
+
 	public static final String GCM_SENDER_ID = "772594394845";
 
 	public static final int EVENT_SYNC_INTERVAL_MILLIS = 60 * 1000;
